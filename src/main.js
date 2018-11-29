f27c90297f3a85dadc49719ec7a9c3b5f7c85812
Tim Rice <trice@troygroup.com>
2018-11-29 09:17:45 -0500

    Downloading .editorconfig template.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
-console.log('It starts')
+const https = require('https')
+const fs = require('fs')
+
+const template = process.argv.slice(2)
+
+function usage() {
+  console.log('usage notes...')
+  process.exit(0)
+}
+
+function download(template, done) {
+  const url = `https://raw.githubusercontent.com/tricertc/editorconfig/master/templates/${template}.editorconfig`
+  https.get(url, res => {
+    let file
+    switch (res.statusCode) {
+      case 200:
+        file = fs.createWriteStream('.editorconfig')
+        res
+          .on('data', chunk => file.write(chunk))
+          .on('end', () => {
+            file.end()
+            done()
+          })
+        break
+      default:
+        done(new Error(`Failed (${res.statusCode}): ${res.statusMessage}`))
+        break
+    }
+  })
+}
+
+if (!template.length) usage()
+
+download(template, err => {
+  if (err) return console.log(err)
+  console.log('.editorconfig created')
+})
